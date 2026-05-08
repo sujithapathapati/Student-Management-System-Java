07affada31185d1964a5789a05f390eec3500f76
sujithapathapati <sujithapathapati678@gmail.com>
2026-05-08 11:22:46 +0530

    Update Main.java

--- main/Main.java
+++ main/Main.java
@@ -6,7 +6,7 @@ import util.InputHelper;
 
 public class Main {
 
-    public static void mai(String[] args) {
+    public static void main(String[] args) {
 
         StudentService service = new StudentService();
 
